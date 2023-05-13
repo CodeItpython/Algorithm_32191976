01e18051050a9fb6f9b9623c9934b2e8beed10af
gunwoo08 <rick9808@gmail.com>
2023-05-13 15:22:35 +0900

    commit 2

--- knapsack_bb.c
+++ knapsack_bb.c
@@ -7,15 +7,15 @@ void knapsack_bb(int i, int weight, int profit) {
     if (weight <= W && profit > maxprofit) {
         maxprofit = profit;
         for (int j = 1; j <= n; j++) {
-            bestx[j] = x[j];
+            bestx[j] = x[j];  // best solution 업데이트
         }
     }
 
     if (i <= n && weight + w[i] <= W) {
-        x[i] = 1;
-        knapsack_bb(i + 1, weight + w[i], profit + p[i]);
-        x[i] = 0;
-        knapsack_bb(i + 1, weight, profit);
+        x[i] = 1;  // 현재 항목 포함
+        knapsack_bb(i + 1, weight + w[i], profit + p[i]);  //포함된 항목과 함께 반복
+        x[i] = 0;  // 현재 항목 제외
+        knapsack_bb(i + 1, weight, profit);  // 제외된 항목으로 반복
     }
 
     int j;
@@ -23,48 +23,50 @@ void knapsack_bb(int i, int weight, int profit) {
     float bound = profit;
     float unit;
 
+    // 나머지 항목을 사용하여 상한 계산
     for (j = i + 1; j <= n; j++) {
         if (totweight + w[j] <= W) {
             totweight += w[j];
             bound += p[j];
         } else {
-            unit = (float)(W - totweight) / w[j];
-            bound += unit * p[j];
+            unit = (float)(W - totweight) / w[j];  //적합한 항목의 부분 계산
+            bound += unit * p[j];  //이익 부분을 한계에 더함
             break;
         }
     }
 
+    //한도가 현재의 최대 이익보다 클 경우 추가로 탐색
     if (bound > maxprofit) {
-        x[i] = 0;
-        knapsack_bb(i + 1, weight, profit);
+        x[i] = 0;  // 현재 항목 제외
+        knapsack_bb(i + 1, weight, profit);  //제외된 항목으로 반복
     }
 }
 
 int main() {
     int i;
-    printf("Enter the number of items: ");
+    printf("Enter the number of items: ");  //아이템들의 수를 입력
     scanf("%d", &n);
-    printf("Enter the capacity of knapsack: ");
+    printf("Enter the capacity of knapsack: "); //capacity(무게)입력
     scanf("%d", &W);
     printf("Enter the weight and profit of each item:\n");
 
     for (i = 1; i <= n; i++) {
         printf("Item %d: ", i);
-        scanf("%d %d", &w[i], &p[i]);
+        scanf("%d %d", &w[i], &p[i]);  //weight와 profit을 각각 입력받는다
     }
 
-    // Using Branch and Bound
+    // Branch and Bound 이용
     printf("Using Branch and Bound:\n");
-    maxprofit = 0;
-    knapsack_bb(0, 0, 0);
-    printf("Maximum profit = %d\n", maxprofit);
+    maxprofit = 0;  // maxprofit 초기화
+    knapsack_bb(0, 0, 0);  //아이템이 없는상태를 포함시킴
+    printf("Maximum profit = %d\n", maxprofit); //maxprofit 출력
     printf("Selected items: ");
     int flag = 0;
     for (i = 1; i <= n; i++) {
-        if (bestx[i]) {
-            if (flag)
+        if (bestx[i]) {  //아이템이 best solution에 있을경우
+            if (flag)   //출력이 안나오는 문제로인한 flag변수 설정
                 printf(", ");
-            printf("%d", i);
+            printf("%d", i);  //선택된 아이템들을 출력
             flag = 1;
         }
     }
--- knapsack_bt.c
+++ knapsack_bt.c
@@ -2,45 +2,42 @@
 // - C언어를 사용하여 구현
 // - 3가지 이상의 입력 데이터로 테스트한 결과를 분석
 // - 주요 자료구조와 알고리즘을 구체적으로 설명
-// - 소스 파일은 보고서와 별도로 제출 (GitHib 링크 제출 가능)
-
+// - 소스 파일은 보고서와 별도로 제출 (GitHib 링크 제출 가능)#include <stdio.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-int n, W, maxprofit;
-int w[100], p[100], x[100], bestx[100];
-
-
+int n, W, maxprofit;  // 품목 수, 배낭 용량 및 최대 이익을 저장하기 위한 변수
+int w[100], p[100], x[100], bestx[100];  // weight, profit, selected item 및 best solution을 저장하는 배열
 void knapsack_bt(int i)
 {
-    if(i > n)
+    if(i > n)  //모든 항목 고려
     {
         int j;
-        int totweight = 0;
-        int totprofit = 0;
+        int totweight = 0;  // selected items의 총 weight
+        int totprofit = 0;  // selected items의 총 profit
         
         for(j=1; j<=n; j++)
         {
-            if(x[j])
+            if(x[j])  // item이 선택됐는지 체크
             {
-                totweight += w[j];
-                totprofit += p[j];
+                totweight += w[j];  // selected item의 weight를 더함
+                totprofit += p[j];  // selected item의 profit을 더함
             }
         }
 
-        if(totweight <= W && totprofit > maxprofit)
+        if(totweight <= W && totprofit > maxprofit)  // solution이 맞고, profit이 적합한지 확인
         {
-            maxprofit = totprofit;
+            maxprofit = totprofit;  // maxprofit 업데이트
             for(j=1; j<=n; j++)
-                bestx[j] = x[j];
+                bestx[j] = x[j];  // best solution 업데이트
         }
     }
     else
     {
-        x[i] = 0;
-        knapsack_bt(i+1);
-        x[i] = 1;
-        knapsack_bt(i+1);
+        x[i] = 0;  // 현재 item 제와
+        knapsack_bt(i+1);  // 제외된 항목들로 반복
+        x[i] = 1;  // 현재 item 포함
+        knapsack_bt(i+1);  // 포함된 항목들로 반복
     }
 }
 
@@ -49,26 +46,27 @@ int main()
 {
     int i;
     printf("Enter the number of items: ");
-    scanf("%d", &n);
+    scanf("%d", &n);  // item의 개수를 입력받음
     printf("Enter the capacity of knapsack: ");
-    scanf("%d", &W);
+    scanf("%d", &W);  // capacity(무게)를 입력받음
     printf("Enter the weight and profit of each item:\n");
 
     for(i=1; i<=n; i++)
     {
         printf("Item %d: ", i);
-        scanf("%d %d", &w[i], &p[i]);
+        scanf("%d %d", &w[i], &p[i]);  //weight과 profit을 각각 입력받음
     }
 
     // Using Backtracking
     printf("Using Backtracking:\n");
-    knapsack_bt(1);
-    printf("Maximum profit = %d\n", maxprofit);
+    maxprofit = 0;  // maxprofit 초기화
+    knapsack_bt(1);  // 첫번째 item으로 시작
+    printf("Maximum profit = %d\n", maxprofit); //maxprofit 출력
     printf("Selected items: ");
     for(i=1; i<=n; i++)
     {
-        if(bestx[i])
-            printf("%d ", i);
+        if(bestx[i])  //best solution에서 item이 선택됐는지 확인
+            printf("%d ", i);  // item들의 index를 출력
     }
     printf("\n");
 
